571b9889eb103918a39ddbc09c19fe9a5982100f
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2022-06-15 10:08:52 +0200

    update paths

--- 02_solve.py
+++ 02_solve.py
@@ -1,12 +1,11 @@
 import csv
+import glob
 import pyscipopt
 import numpy as np
 
-
-n_instances = 1
-problem = 'mknapsack'
+problem = 'MIPLIB_subset'
+instances = glob.glob(f'benchmarks/{problem}/ahl_*.lp')
 result_file = f'results/{problem}.csv'
-instance_path = f'benchmarks/{problem}'
 
 formulations = ['ahl', 'orig']
 fieldnames = ['instance', 'seed', 'formulation', 'nnodes', 'time', 'status']
@@ -27,20 +26,23 @@ with open(result_file, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    for i in range(n_instances):
+    for instance in instances:
         optimalsol = None
+        name = instance.split('/')[-1].split('ahl_')[-1].split('.')[0]
+
         for seed in range(4):
-            print(f"~~ Instance {i+1}, seed {seed}")
+            print(f"~~ Instance {instance}, seed {seed}")
             for formulation in formulations:
                 print(f"    Formulation {formulation}")
-                name = f'instance_{i+1}_100_6.lp'
-                if formulation == "ahl": name = f'ahl_instance_{i+1}_100_6.lp'
-                results = {'instance': f'instance_{i+1}',
+                results = {'instance': name,
                            'formulation': formulation,
                            'seed': seed }
+                
+                if formulation == "ahl": 
+                    m.readProblem(instance)
+                else:
+                    m.readProblem(instance.replace("ahl_", "").replace(".lp",".mps"))
 
-
-                m.readProblem(f'{instance_path}/{name}')
                 m.setParam('randomization/permutationseed', seed)
                 m.setParams(scip_parameters)
 
