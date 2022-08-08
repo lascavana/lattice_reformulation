2abc381a483832fd6861616ab078bda4a8736a7b
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2022-08-08 15:07:51 +0200

    some more analysis tools

--- 04_analyze.py
+++ 04_analyze.py
@@ -1,7 +1,49 @@
-import pandas as pd
+import scipy
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats.mstats import gmean
+# from scipy.stats.mstats import gmean
+
+def gmean(x):
+    x = np.array(x)
+    if np.where(x==0.0)[0].size > 0:
+        return 0.0
+    else:
+        a = np.log(x)
+        return np.exp(a.mean())
+
+def MIPLIB_table():
+    data = pd.read_csv("results/MIPLIB_subset_all.csv")
+    results = {}
+
+    instances = list(set(data['instance']))
+    instances.sort()
+    seeds = set(data['seed'])
+    formulations = set(data['formulation'])
+
+    for instance in instances:
+        i_data = data.loc[data['instance'] == instance]
+        nnodes = {'nnodes_ahl':[], 'nnodes_orig': []}
+        for seed in seeds:
+            is_data = i_data.loc[i_data['seed'] == seed]
+            for formulation in formulations:
+                isf_data = is_data.loc[is_data['formulation'] == formulation]
+                nnodes[f'nnodes_{formulation}'].append((isf_data['nnodes'].values[0]))
+
+        results[instance] = {'nnodes_ahl_mean': np.mean(nnodes['nnodes_ahl']),
+                            'nnodes_orig_mean': np.mean(nnodes['nnodes_orig']),
+                            'nnodes_ahl_gmean': gmean(nnodes['nnodes_ahl']),
+                            'nnodes_orig_gmean': gmean(nnodes['nnodes_orig']),
+                            'nnodes_ahl_std': np.std(nnodes['nnodes_ahl']),
+                            'nnodes_orig_std': np.std(nnodes['nnodes_orig']),
+                            'nnodes_ahl': nnodes['nnodes_ahl'],
+                            'nnodes_orig': nnodes['nnodes_orig']}
+    
+        print(instance, 
+              "{:.2f}".format(results[instance]['nnodes_orig_gmean']), 
+              " & ",
+              "{:.2f}".format(results[instance]['nnodes_ahl_gmean']) )
+
 
 
 def box_plot(ax, data, edge_color, fill_color):
@@ -18,7 +60,10 @@ def box_plot(ax, data, edge_color, fill_color):
 
     return bp
 
-data = pd.read_csv("results/results_mknapsack.csv")
+
+MIPLIB_table()
+assert 0
+data = pd.read_csv("results/results_mknapsack_nontrans.csv")
 results = {}
 
 instances = set(data['instance'])
@@ -50,8 +95,11 @@ ind = np.argsort(orig_mean)
 
 orig = [results[instance]['nnodes_orig'] for instance in instances]
 ahl = [results[instance]['nnodes_ahl'] for instance in instances]
+determinants = [determinants[instance] for instance in instances]
 orig = [orig[j] for j in ind]
 ahl = [ahl[j] for j in ind]
+determinants = [determinants[j] for j in ind]
+print(determinants)
 
 fig, ax = plt.subplots()
 
@@ -61,7 +109,7 @@ bp2 = box_plot(ax, ahl, 'blue', (0, 0, 1, .3))
 ax.legend([bp1["boxes"][0], bp2["boxes"][0]], ['Original', 'Reformulated'])
 
 # show plot
-plt.title('Market split (m=4)')
+plt.title('Multiple knapsack')
 plt.xticks([])
 plt.xlabel('Instance')
 plt.yscale('log')
@@ -72,49 +120,49 @@ plt.show()
 
 
 ## Same but with time ##
-instances = set(data['instance'])
-seeds = set(data['seed'])
-formulations = set(data['formulation'])
-
-for instance in instances:
-    i_data = data.loc[data['instance'] == instance]
-    time = {'time_ahl':[], 'time_orig': []}
-    for seed in seeds:
-        is_data = i_data.loc[i_data['seed'] == seed]
-        for formulation in formulations:
-            isf_data = is_data.loc[is_data['formulation'] == formulation]
-            time[f'time_{formulation}'].append((isf_data['time'].values[0]))
-
-    results[instance] = {'time_ahl_mean': np.mean(time['time_ahl']),
-                         'time_orig_mean': np.mean(time['time_orig']),
-                         'time_ahl_gmean': gmean(time['time_ahl']),
-                         'time_orig_gmean': gmean(time['time_orig']),
-                         'time_ahl_std': np.std(time['time_ahl']),
-                         'time_orig_std': np.std(time['time_orig']),
-                         'time_ahl': time['time_ahl'],
-                         'time_orig': time['time_orig']}
-
-
-
-orig_mean = [results[instance]['time_orig_mean'] for instance in instances]
-ind = np.argsort(orig_mean)
-
-orig = [results[instance]['time_orig'] for instance in instances]
-ahl = [results[instance]['time_ahl'] for instance in instances]
-orig = [orig[j] for j in ind]
-ahl = [ahl[j] for j in ind]
-
-fig, ax = plt.subplots()
-
-# Creating plot
-bp1 = box_plot(ax, orig, 'red', (1, 0, 0, .3))
-bp2 = box_plot(ax, ahl, 'blue', (0, 0, 1, .3))
-ax.legend([bp1["boxes"][0], bp2["boxes"][0]], ['Original', 'Reformulated'])
-
-# show plot
-plt.title('Market split (m=4)')
-plt.xticks([])
-plt.xlabel('Instance')
-plt.yscale('log')
-plt.ylabel('Time')
-plt.show()
+# instances = set(data['instance'])
+# seeds = set(data['seed'])
+# formulations = set(data['formulation'])
+#
+# for instance in instances:
+#     i_data = data.loc[data['instance'] == instance]
+#     time = {'time_ahl':[], 'time_orig': []}
+#     for seed in seeds:
+#         is_data = i_data.loc[i_data['seed'] == seed]
+#         for formulation in formulations:
+#             isf_data = is_data.loc[is_data['formulation'] == formulation]
+#             time[f'time_{formulation}'].append((isf_data['time'].values[0]))
+#
+#     results[instance] = {'time_ahl_mean': np.mean(time['time_ahl']),
+#                          'time_orig_mean': np.mean(time['time_orig']),
+#                          'time_ahl_gmean': gmean(time['time_ahl']),
+#                          'time_orig_gmean': gmean(time['time_orig']),
+#                          'time_ahl_std': np.std(time['time_ahl']),
+#                          'time_orig_std': np.std(time['time_orig']),
+#                          'time_ahl': time['time_ahl'],
+#                          'time_orig': time['time_orig']}
+#
+#
+#
+# orig_mean = [results[instance]['time_orig_mean'] for instance in instances]
+# ind = np.argsort(orig_mean)
+#
+# orig = [results[instance]['time_orig'] for instance in instances]
+# ahl = [results[instance]['time_ahl'] for instance in instances]
+# orig = [orig[j] for j in ind]
+# ahl = [ahl[j] for j in ind]
+#
+# fig, ax = plt.subplots()
+#
+# # Creating plot
+# bp1 = box_plot(ax, orig, 'red', (1, 0, 0, .3))
+# bp2 = box_plot(ax, ahl, 'blue', (0, 0, 1, .3))
+# ax.legend([bp1["boxes"][0], bp2["boxes"][0]], ['Original', 'Reformulated'])
+#
+# # show plot
+# plt.title('Multiple knapsack')
+# plt.xticks([])
+# plt.xlabel('Instance')
+# plt.yscale('log')
+# plt.ylabel('Time')
+# plt.show()
