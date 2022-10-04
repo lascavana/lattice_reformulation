06058951b0bdc01ef8e9827289d9596199564846
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2022-10-04 15:55:58 +0200

    rename

--- 01_reformulate.sh
+++ 01_reformulate.sh
@@ -2,7 +2,7 @@
 rm -r build
 mkdir build
 cd build
-cmake .. -DSCIP_DIR=/Users/lvscavuzzomont/opt/scipoptsuite-8.0.0
+cmake .. -DSCIP_DIR=$SCIPOPTDIR
 make
 cd ..
 
@@ -21,10 +21,10 @@ cd ..
 #   ./build/reformulate benchmarks/prob_struct/prob${i}.lp settingsfile.set
 # done
 
-for i in {11..20}
-do
-  ./build/reformulate benchmarks/prob_nostruct/prob${i}.lp settingsfile.set
-done
+# for i in {11..20}
+# do
+#   ./build/reformulate benchmarks/prob_nostruct/prob${i}.lp settingsfile.set
+# done
 
 # for i in {1..50}
 # do
@@ -35,3 +35,5 @@ done
 # do
 #   ./build/reformulate benchmarks/msplit_feasibility/instance_${i}.lp settingsfile.set
 # done
+
+./build/reformulate benchmarks/cuww/instance_1.lp kc settingsfile.set
