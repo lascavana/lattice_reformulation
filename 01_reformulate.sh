23bb6e1504c891c9f7fde25a6c271d42b57b2928
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2024-03-12 14:06:31 +0100

    add miplib instructions

--- 01_reformulate.sh
+++ 01_reformulate.sh
@@ -9,64 +9,9 @@ cd ..
 rm -r logs
 mkdir logs
 
-
-# struct_s
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/struct_s/instance_${i}.lp ahl settingsfile.set >> logs/struct_s_ahl.txt
-  ./build/reformulate benchmarks/struct_s/instance_${i}.lp ahl_diag settingsfile.set >> logs/struct_s_ahl_diag.txt
-  ./build/reformulate benchmarks/struct_s/instance_${i}.lp ahl_poor settingsfile.set >> logs/struct_s_ahl_poor.txt
-  ./build/reformulate benchmarks/struct_s/instance_${i}.lp kp settingsfile.set >> logs/struct_s_kp.txt
+# MIPLIB
+for i in benchmarks/MIPLIB/*.mps; do
+    echo $i
+    ./build/reformulate $i ahl settingsfile.set
+    ./build/reformulate $i kp settingsfile.set
 done
-
-# struct_b
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/struct_b/instance_${i}.lp ahl settingsfile.set >> logs/struct_b_ahl.txt
-  ./build/reformulate benchmarks/struct_b/instance_${i}.lp ahl_diag settingsfile.set >> logs/struct_b_ahl_diag.txt
-  ./build/reformulate benchmarks/struct_b/instance_${i}.lp ahl_poor settingsfile.set >> logs/struct_b_ahl_poor.txt
-  ./build/reformulate benchmarks/struct_b/instance_${i}.lp kp settingsfile.set >> logs/struct_b_kp.txt
-done
-
-# nostruct_s
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl settingsfile.set >> logs/nostruct_s_ahl.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl_diag settingsfile.set >> logs/nostruct_s_ahl_diag.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl_poor settingsfile.set >> logs/nostruct_s_ahl_poor.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp kp settingsfile.set >> logs/nostruct_s_kp.txt
-done
-
-# nostruct_b
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl settingsfile.set >> logs/nostruct_b_ahl.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl_diag settingsfile.set >> logs/nostruct_b_ahl_diag.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp ahl_poor settingsfile.set >> logs/nostruct_b_ahl_poor.txt
-  ./build/reformulate benchmarks/nostruct_s/instance_${i}.lp kp settingsfile.set >> logs/nostruct_b_kp.txt
-done
-
-# Market split
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/MS/instance_${i}.lp ahl settingsfile.set >> logs/ms_ahl.txt
-  ./build/reformulate benchmarks/MS/instance_${i}.lp ahl_poor settingsfile.set >> logs/ms_ahl_poor.txt
-  ./build/reformulate benchmarks/MS/instance_${i}.lp kp settingsfile.set >> logs/ms_kp.txt
-done
-
-# Generalized assignment 
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/GAP/instance_${i}.lp ahl settingsfile.set >> logs/gap_ahl.txt
-  ./build/reformulate benchmarks/GAP/instance_${i}.lp ahl_poor settingsfile.set >> logs/gap_ahl_poor.txt
-  ./build/reformulate benchmarks/GAP/instance_${i}.lp kp settingsfile.set >> logs/gap_kp.txt
-done
-
-# Combinatorial auctions
-for i in {1..30}
-do
-  ./build/reformulate benchmarks/CA/instance_${i}.lp ahl settingsfile.set >> logs/ca_ahl.txt
-  ./build/reformulate benchmarks/CA/instance_${i}.lp ahl_poor settingsfile.set >> logs/ca_ahl_poor.txt
-  ./build/reformulate benchmarks/CA/instance_${i}.lp kp settingsfile.set >> logs/ca_kp.txt
-done
-
